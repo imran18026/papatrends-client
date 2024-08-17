146d80978ba9d3da1c8beed0ea4fc833af8c1681
Md Imran Hossain <mdimranhossain.ist@gmail.com>
2024-08-17 14:16:42 +0600

    add Image

--- src/lib/getall.ts
+++ src/lib/getall.ts
@@ -1,5 +1,7 @@
 const getAllBlog = async () => {
-  const res = await fetch("https://papatrends-server.vercel.app/api/v1/blog");
+  const res = await fetch("https://papatrends-server.vercel.app/api/v1/blog", {
+    next: { revalidate: 3600 },
+  });
   const data = await res.json();
 
   return data;
